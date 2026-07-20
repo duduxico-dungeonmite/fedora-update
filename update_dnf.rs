15894a43c8ba406aef7b349ef210adb601f61339
Eduardo Francisco Bertoli Mancia <82223566+duduxico-dungeonmite@users.noreply.github.com>
2026-07-20 17:05:28 -0300

    Update update_dnf.rs

--- update_dnf.rs
+++ update_dnf.rs
@@ -1,23 +1,5 @@
-use std::process::Command;
+use crate::command::run_command;
 
-//noinspection ALL,RsUnresolvedPath
 pub fn update_dnf() {
-    let update_msg = "Updating DNF...";
-
-    // REMEMBER: when you release, remove "sudo" and only make:
-    // Command::new("dnf5) | .args(["upgrade", "-y"]) | .status();
-    // if someone reads this doc, every | = empty space/new line.
-    // please remember! ><
-
-    println!("{}", update_msg);
-
-    let status = Command::new("sudo")
-        .args(["dnf5", "upgrade", "-y"])
-        .status();
-    match status {
-        Ok(s) if s.success() => println!("DNF update finished successfully."),
-        Ok(s) => eprintln!("dnf5 exited with status: {}", s),
-        Err(e) => eprintln!("Failed to run dnf5: {}", e),
-    }
+   run_command("Updating DNF...", "sudo",&["dnf5","update", "-y"])
 }
-
