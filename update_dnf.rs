6fbdbc7548484ca5becec2adb977d51238e8d741
Eduardo Francisco Bertoli Mancia <82223566+duduxico-dungeonmite@users.noreply.github.com>
2026-07-20 22:29:22 -0300

    Update update_dnf.rs

--- update_dnf.rs
+++ update_dnf.rs
@@ -1,5 +1,5 @@
 use crate::command::run_command;
 
 pub fn update_dnf() {
-   run_command("Updating DNF...", "sudo",&["dnf5","update", "-y"])
+   run_command("Updating DNF...", "dnf",&["update", "-y"])
 }
